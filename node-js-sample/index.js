b30debe6f17f24dc4fbdfdea8fc15856591643f4
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2015-05-28 18:06:21 -0400

    Updated the nodejs project by removing logentries

--- node-js-sample/index.js
+++ node-js-sample/index.js
@@ -11,16 +11,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-var logentries = require('./')({
-  json: false, // or true to parse lines as JSON
-  secure: false, // or true to connect securely
-  token: process.env.e062dc9f-05ef-4fa8-b217-c7fcd38c9e9a
-});
-
-// logentries is the source stream with all the
-// log lines
-
-setTimeout(function() {
-  logentries.destroy()
-}, 1000);
